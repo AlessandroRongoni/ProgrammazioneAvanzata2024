eadc312e00a41727c824337216fb299d8e49b47d
Alessandro Rongoni <74723429+AlessandroRongoni@users.noreply.github.com>
2024-02-14 11:04:57 +0100

    docker parte

--- backend/node/src/models/EdgeModel.ts
+++ backend/node/src/models/EdgeModel.ts
@@ -1,37 +1,35 @@
-// src/models/EdgeModel.ts
+// import { DataTypes } from 'sequelize';
+// import { sequelize } from '../config/sequelize';
+// import { Graph } from './GraphModel';
 
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/sequelize';
-import { Graph } from './GraphModel';
+// export const Edge = sequelize.define('Edge', {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
+//   graphId: {
+//     type: DataTypes.INTEGER,
+//     references: {
+//       model: Graph,
+//       key: 'id',
+//     },
+//   },
+//   startNode: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   endNode: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   weight: {
+//     type: DataTypes.FLOAT,
+//     allowNull: false,
+//   },
+// }, {
+//   tableName: 'edges',
+// });
 
-export const Edge = sequelize.define('Edge', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  graphId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Graph,
-      key: 'id',
-    },
-  },
-  startNode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  endNode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  weight: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-}, {
-  tableName: 'edges',
-});
-
-Graph.hasMany(Edge);
-Edge.belongsTo(Graph);
+// Graph.hasMany(Edge);
+// Edge.belongsTo(Graph);
