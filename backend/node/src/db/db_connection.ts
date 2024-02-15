af4b932a582ab23ae7a0024686abced6c5224152
Alessandro Rongoni <74723429+AlessandroRongoni@users.noreply.github.com>
2024-02-15 11:24:40 +0100

    aggiustamenti db

--- backend/node/src/db/db_connection.ts
+++ backend/node/src/db/db_connection.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 // Carica le variabili d'ambiente dal file .env
 dotenv.config();
 
-const dbUsername = process.env.PGUSER || 'user'; //
-const dbPassword = process.env.PGPASSWORD; // Assicurati di avere questa variabile definita nel tuo .env
-const dbName = process.env.PGDATABASEE || 'my_db'; // Fornisce un valore di default se MYSQL_DB_NAME non è definito
-const dbHost = process.env.PGHOST || 'db_host'; // Fornisce un valore di default se MYSQL_HOST non è definito
-const dbPort = process.env.PGPORT || '5432'; // Fornisce un valore di default se MYSQL_PORT non è definito
+// const dbUsername = process.env.PGUSER || 'user'; //
+// const dbPassword = process.env.PGPASSWORD; // Assicurati di avere questa variabile definita nel tuo .env
+// const dbName = process.env.PGDATABASEE || 'my_db'; // Fornisce un valore di default se MYSQL_DB_NAME non è definito
+// const dbHost = process.env.PGHOST || 'db_host'; // Fornisce un valore di default se MYSQL_HOST non è definito
+// const dbPort = process.env.PGPORT || '5432'; // Fornisce un valore di default se MYSQL_PORT non è definito
 /**
  * Classe per la connessione al database.
  * Utilizza il pattern Singleton per garantire una sola istanza di connessione.
@@ -17,8 +17,8 @@ export class DbConnector {
     private static instance: DbConnector;
     private sequelizer: any;
     private constructor() {
-        this.sequelizer = new Sequelize(dbName, dbUsername, dbPassword, {
-            host: dbHost,
+        this.sequelizer = new Sequelize('my_db', 'postgres', 'postgres', {
+            host: 'db',
             dialect: 'postgres',
         });
     }
